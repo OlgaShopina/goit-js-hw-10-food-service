6a6ffe0d0a2e5333c4671fd8d8309834e3b72857
OlgaShopina <olga.shopina@gmail.com>
2021-07-31 15:17:19 +0300

    url

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import menu from './menu.json';
 import templates from './templates/templates.hbs';
 
 
-
 const menuEl = document.querySelector('.js-menu');
 const menuCards = menuGallery(menu);
 menuEl.insertAdjacentHTML('beforeend', menuCards);
